3b5ecb4b36515a9eba7a8985fed2e45750e05097
Michael Cordell <mike@mikecordell.com>
2023-06-22 20:52:21 -0700

    Tidy calendar spec

--- spec/tty/calendar_spec.rb
+++ spec/tty/calendar_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Tty::Calendar do
-  it "has a version number" do
-    expect(Tty::Calendar::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe TTY::Calendar do
+  it 'has a version number' do
+    expect(TTY::Calendar::VERSION).not_to be nil
   end
 end
